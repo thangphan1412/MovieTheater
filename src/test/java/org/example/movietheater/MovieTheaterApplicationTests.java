8f83c727a9d21f4bea8d42a5f4e5e70d2b80ac85
thangphan14122003@gmail.com <thangphan14122003@gmail.com>
2025-02-08 21:08:55 +0700

    first commit

---
+++ src/test/java/org/example/movietheater/MovieTheaterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.movietheater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieTheaterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
