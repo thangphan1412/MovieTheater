f8260bb933bfd751a6c0add43c72608590f424ad
thangphan14122003@gmail.com <thangphan14122003@gmail.com>
2025-04-12 01:45:12 +0700

    setup reactjs

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@Controller
+@RestController
 public class HomeController {
-    @GetMapping("/")
+    @GetMapping("/home")
     public String home() {
 
         return "index";
---
+++ src/main/java/security/jwt/JWTConfig.java
@@ -0,0 +1,4 @@
+package security.jwt;
+
+public class JWTConfig {
+}
